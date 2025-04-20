4e0790a604d29c45ddcbcea644a71e2b24cf1f83
Worrakamol <6531501169@lamduan.mfu.ac.th>
2025-04-20 15:47:47 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 20 15:43:46 2025
+
+@author: LAB
+"""
+
+import streamlit as st
+import pickle
+import matplotlib.pyplot as plt
+from sklearn.datasets import make_blobs
+
+# Load model
+with open('kmeans_model.pkl', 'rb') as f:
+    loaded_model = pickle.load(f)
+
+# Page config
+st.set_page_config(page_title="k-Means Clustering App", layout="centered")
+st.title("🔍 k-Means Clustering Visualizer")
+st.subheader("📊 Example Data for Visualization")
+st.markdown("This demo uses example data (2D) to illustrate clustering results.")
+
+# Generate data
+X, _ = make_blobs(n_samples=300, centers=loaded_model.n_clusters, cluster_std=0.60, random_state=0)
+
+# Predict
+y_kmeans = loaded_model.predict(X)
+
+# Plot
+plt.scatter(X[:, 0], X[:, 1], c=y_kmeans, s=50, cmap='viridis')
+centers = loaded_model.cluster_centers_
+plt.scatter(centers[:, 0], centers[:, 1], c='red', s=200, alpha=0.75)
+st.pyplot(plt)
